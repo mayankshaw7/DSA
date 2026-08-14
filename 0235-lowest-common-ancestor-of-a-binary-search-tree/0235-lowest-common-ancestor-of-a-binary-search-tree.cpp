2bb768944ca3f9beb31633a8022cb5bc2811295e
Mayank Shaw <mayankshaw4001@gmail.com>
2026-08-14 21:02:56 +0530

    Time: 24 ms (49.17%), Space: 23.5 MB (66.59%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,7 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
+//used Similar Logic of LCA of BT 
+/*
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -22,6 +23,27 @@ public:
         if(right==NULL) return left;
         //it means that root got the both values right and left 
         //and it the onyl lower common ancester of both the root 
+        return root;
+    }
+};
+*/
+//Solving using the Optimized Approach 
+//By checking The greater and less than the root methodology
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==NULL){
+            return root;
+        }
+        int curr=root->val;
+        if(curr<p->val && curr<q->val){
+            return lowestCommonAncestor(root->right,p,q);
+        }
+        if(curr>p->val && curr>q->val){
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        //else it means it is lieing the middle of on left p and on right they have q;
+        //also 1 se 1 bhi possible hai isme means agar sirf p and q diya ho toh 
         return root;
     }
 };
