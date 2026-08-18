ef25a8a1b3f4359132cbd7579408c42277665fd0
Mayank Shaw <mayankshaw4001@gmail.com>
2026-08-18 20:26:47 +0530

    Time: 0 ms (100%), Space: 17.5 MB (78.58%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -6,29 +6,58 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
- //optimized approach ..i did this with my own 
+// optimized approach ..i did this with my own
+// Morries Traversal Is damn easy man
+
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        TreeNode* curr=root;
+        TreeNode* curr = root;
 
-        while(curr!=NULL){
-            if(curr->left==NULL){
-                curr=curr->right;
-            }else{
-            TreeNode* leftchild=curr->left;
-              while(leftchild->right!=NULL){
-                leftchild=leftchild->right;
-              }
-              if(leftchild->right==NULL){
-                leftchild->right=curr->right;
-                curr->right=curr->left;
-                curr->left=NULL;
-              }
+        while (curr != NULL) {
+            if (curr->left == NULL) {
+                curr = curr->right;
+            } else {
+                TreeNode* leftchild = curr->left;
+                while (leftchild->right != NULL) {
+                    leftchild = leftchild->right;
+                }
+                if (leftchild->right == NULL) {
+                    leftchild->right = curr->right;
+                    curr->right = curr->left;
+                    curr->left = NULL;
+                }
             }
         }
     }
 };
+
+// Recursive Solution
+// Just solve one problem and everything will be done by recursion have faith in
+// it has a stack space of o(n)
+/*
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root)
+            return;
+        TreeNode* templeft = root->left;
+        TreeNode* tempright = root->right;
+        flatten(root->left);
+        flatten(root->right);
+
+        root->left = NULL;
+        root->right = templeft;
+        TreeNode* curr = root;
+        while (curr->right != NULL) {
+            curr = curr->right;
+        }
+        if (curr->right == NULL)
+            curr->right = tempright;
+    }
+};
+*/
