bc8d30053bac8a34353a5b4cf86115c7ca18bdda
Mayank Shaw <mayankshaw4001@gmail.com>
2026-08-30 17:37:39 +0530

    Time: 666 ms (14.64%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,9 +1,14 @@
 # Write your MySQL query statement below
-select name from
-((select id,name from customer
-where referee_id!=2)
-UNION
-(select id,name from customer
-where referee_id is null)) as t
-order by id;
-
+/*
+-- brtue force
+-- select name from
+-- ((select id,name from customer
+-- where referee_id!=2)
+-- UNION
+-- (select id,name from customer
+-- where referee_id is null)) as t
+-- order by id;
+*/
+SELECT name 
+FROM Customer 
+WHERE referee_id != 2 OR referee_id IS NULL;
