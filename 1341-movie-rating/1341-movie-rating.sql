232348cd4f905ee7b4ac0ddecf47d3e9ce244681
Mayank Shaw <mayankshaw4001@gmail.com>
2026-09-09 21:03:50 +0530

    Time: 1481 ms (52.76%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,19 +1,35 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
+-- (
+-- select u.name as results from users u join movierating mr 
+-- On u.user_id = mr.user_id
+-- group by u.user_id
+-- order by count(*) DESC,u.name
+-- limit 1
+-- )
+-- UNION ALL
+-- (
+-- select m.title as results from Movies m join movierating mr 
+-- On m.movie_id = mr.movie_id
+-- where mr.created_at  between '2020-02-01' AND '2020-02-29'
+-- group by m.movie_id,m.title
+
+-- order by AVG(mr.rating) DESC,m.title
+-- limit 1
+-- );
+-- where movierating.created_at between '2020-02-01' AND '2020-02-29' )\
 (
-select u.name as results from users u join movierating mr 
-On u.user_id = mr.user_id
+select u.name as results from users as u
+join movierating as mr on u.user_id=mr.user_id
 group by u.user_id
-order by count(*) DESC,u.name
+order by count(mr.user_id) desc,u.name
 limit 1
 )
 UNION ALL
 (
-select m.title as results from Movies m join movierating mr 
-On m.movie_id = mr.movie_id
-where mr.created_at  between '2020-02-01' AND '2020-02-29'
-group by m.movie_id,m.title
-
-order by AVG(mr.rating) DESC,m.title
+select m.title as results from movies as m
+join movierating as mr on m.movie_id=mr.movie_id
+where year(mr.created_at)=2020 and month(mr.created_at)=2
+group by m.movie_id
+order by avg(mr.rating) desc,m.title
 limit 1
 );
--- where movierating.created_at between '2020-02-01' AND '2020-02-29' )
