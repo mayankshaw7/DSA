bf4ef7272401b041dd9a0e93c6efe64277d642f5
Mayank Shaw <mayankshaw4001@gmail.com>
2026-09-11 11:02:02 +0530

    Time: 0 ms (100%), Space: 42 MB (65.25%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,37 +1,49 @@
-// class Solution {
-//     public int uniquePaths(int m, int n) {
-//         //solving using dp only 
-//         int [][]dp=new int[m+1][n+1];
-//         dp[0][0]=1;
-//         for(int i=0;i<=m;i++){
-//             for(int j=0;j<=n;j++){
-//                 if(i==0 || j==0) 
-//             }
-//         }
-//         return dp[m][n];
-//     }
-// }
+//pure dp approach 
+class Solution {
+    public int uniquePaths(int m, int n) {
+        //solving using dp only 
+        int[][] dp = new int[m][n];
+
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                int down = 0, right = 0;
+                if (i > 0)
+                    down = dp[i - 1][j];
+                if (j > 0)
+                    right = dp[i][j - 1];
+                dp[i][j] = down + right;
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
+/*
 //Recursion + memoization call here in t
 
 class Solution {
     public int uniquePaths(int m, int n) {
         //solving using dp only 
-        int [][]dp=new int[m][n];
-        for(int []row:dp)
-        Arrays.fill(row,-1);
-        dp[0][0]=1;
-       
-        return f(dp,m-1,n-1);
-
+        int[][] dp = new int[m][n];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        dp[0][0] = 1;
+        return f(dp, m - 1, n - 1);
     }
-    private int f(int [][]dp,int i,int j){
-        if(i<0 || j<0) return 0;
 
-        int up=0,left=0;
-        if(dp[i][j]!=-1) return dp[i][j];
+    private int f(int[][] dp, int i, int j) {
+        if (i < 0 || j < 0)
+            return 0;
 
-        up = f(dp,i-1,j);
-        left = f(dp,i,j-1);
-        return dp[i][j]=up +left;
+        int up = 0, left = 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        up = f(dp, i - 1, j);
+        left = f(dp, i, j - 1);
+        return dp[i][j] = up + left;
     }
 }
+*/
