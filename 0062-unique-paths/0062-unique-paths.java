cbf69ca3522b3d6fa8403d4e3619d5ef4de2728e
Mayank Shaw <mayankshaw4001@gmail.com>
2026-09-11 10:50:56 +0530

    Time: 0 ms (100%), Space: 42.4 MB (12.7%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,37 @@
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//         //solving using dp only 
+//         int [][]dp=new int[m+1][n+1];
+//         dp[0][0]=1;
+//         for(int i=0;i<=m;i++){
+//             for(int j=0;j<=n;j++){
+//                 if(i==0 || j==0) 
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// }
+//Recursion + memoization call here in t
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        //solving using dp only 
+        int [][]dp=new int[m][n];
+        for(int []row:dp)
+        Arrays.fill(row,-1);
+        dp[0][0]=1;
+       
+        return f(dp,m-1,n-1);
+
+    }
+    private int f(int [][]dp,int i,int j){
+        if(i<0 || j<0) return 0;
+
+        int up=0,left=0;
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        up = f(dp,i-1,j);
+        left = f(dp,i,j-1);
+        return dp[i][j]=up +left;
+    }
+}
