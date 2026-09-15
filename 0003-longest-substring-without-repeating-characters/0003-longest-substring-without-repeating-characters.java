c28f49e2e4910ae07532e49ae7b39db904aa92d3
Mayank Shaw <mayankshaw4001@gmail.com>
2026-09-15 20:45:34 +0530

    Time: 6 ms (90.05%), Space: 47.7 MB (67.68%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,3 +1,4 @@
+/* Also can be done using the hashset 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         //using the sliiding window + haspmap
@@ -17,6 +18,34 @@ class Solution {
             }
             j++;
         }
+        return longest_size;
+    }
+}
+*/
+//Optimized approach
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // Direct array lookup for ASCII characters (size 128)
+        int[] lastIndex = new int[128];
+        
+        // Fill array with -1 (or 0-based offsets)
+        java.util.Arrays.fill(lastIndex, -1);
+        
+        int longest_size = 0;
+        int i = 0; // Left boundary of sliding window
+        
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            
+            // If character was seen before inside current window, move left pointer
+            if (lastIndex[c] >= i) {
+                i = lastIndex[c] + 1;
+            }
+            
+            lastIndex[c] = j; // Update last seen index
+            longest_size = Math.max(longest_size, j - i + 1);
+        }
+        
         return longest_size;
     }
 }
